9a19b6f0e89df378642a52b5d12a40ac76ec118e
Muhammad Wasif <76221199+wiznemm@users.noreply.github.com>
2023-07-07 12:39:48 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ShoppingList from './slist';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Shopping App</h1>
+      <ShoppingList />
+    </div>
+  );
+};
+
+export default App;
